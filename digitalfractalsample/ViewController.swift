25c6fa29b15d60a8f1d0f7f7777d804263c136ba
salman <nmssalman@outlook.com>
2020-07-29 23:09:53 +0900

    init

---
+++ digitalfractalsample/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  digitalfractalsample
+//
+//  Created by Mohamed Salman on 2020/07/28.
+//  Copyright © 2020 Mohamed Salman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var textLabel: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
+        textLabel.text = "Application Version Code: " + appVersion!
+        
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
