a863a5d8cf6ba1ddc6654f479589f6e80bad4147
salman <nmssalman@outlook.com>
2020-07-29 23:16:54 +0900

    fx

--- digitalfractalsample/ViewController.swift
+++ digitalfractalsample/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         let appVersion = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
-        textLabel.text = "Application Version Code: " + appVersion!
+        textLabel.text = "Application Version Code Number: " + appVersion!
         
         // Do any additional setup after loading the view.
     }
